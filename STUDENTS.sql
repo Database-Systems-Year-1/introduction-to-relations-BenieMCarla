19acf71e2d39886f2ed89449460255a487fcac20
BenieMCarla <167401585+BenieMCarla@users.noreply.github.com>
2024-09-10 13:40:10 +0300

    Add files via upload

---
+++ STUDENTS.sql
@@ -0,0 +1,10 @@
+CREATE TABLE students 
+            (id INT),
+		     name VARCHAR(50),
+			 age INT,
+			 year INT,
+			 semester INT,
+			 primary key(id));
+
+insert into students(id,name,age,year,semester)
+VALUES(152890,'CARLA',3,5,7)
